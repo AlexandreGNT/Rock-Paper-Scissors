708c639bc1b52f3c1005746e668596082a7257b4
AlexandreGNT <stoapofftrack@gmail.com>
2024-02-24 12:39:54 +0100

    Can console log player and computer choices

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const container = document.querySelector('#container')
 const buttons = document.querySelectorAll('button')
-
-function playerSelection(event) {
+function getComputerChoice(){
+    let choicesArray = ['Rock','Paper','Scissors'];
+    let randomIndex = Math.floor(Math.random() * choicesArray.length);
+    let computerChoice = choicesArray[randomIndex];
+    return computerChoice;
+}
+function getPlayerChoice(event){
     let playerChoice = event.target.innerText
-    console.log("Player selected: " + playerChoice)
+    return playerChoice;
+}
+function getBothChoices(event){
+    let playerSelection = getPlayerChoice(event);
+    let computerSelection = getComputerChoice();
+    return {
+        playerChoice: playerSelection,
+        computerChoice: computerSelection
+    };
+}
+function showBothChoices(bothChoices){
+    console.log("Player Choice: " + bothChoices.playerChoice);
+    console.log("Computer Choice : " + bothChoices.computerChoice);
+    
 }
 buttons.forEach((button) => {
-    button.addEventListener('click',playerSelection)    
+    button.addEventListener('click',function(event){
+         // Call myFunction and pass the event object
+    var Choices = getBothChoices(event);
+    
+    // Call handleValues and pass the returned values
+    showBothChoices(Choices);
+    })    
 });
