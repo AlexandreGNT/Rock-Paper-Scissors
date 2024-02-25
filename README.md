0520e454d419d3dc48fe14e6ea6f94774a55d1ce
AlexandreGNT <stoapofftrack@gmail.com>
2024-02-25 12:17:40 +0100

    Can click and display score

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ JS RPS from Odin Project
 
 1] Begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 2] Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner.
+
+
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+// DEFINE FOR THE JS
 const container = document.querySelector('#container')
+container.classList.add('container')
 const buttons = document.querySelectorAll('button')
+// DEFINE AND CREATE THE STRUCTURE
+// CREATE THE BUTTON CONTAINER
+const button_container = document.createElement('div')
+button_container.classList.add('btn-container')
+// DEFINE EACH BUTTONS INDIVIDUALLY AND APPEND TO BUTTON_CONTAINER
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
+button_container.appendChild(rock)
+button_container.appendChild(paper)
+button_container.appendChild(scissors)
+// APPEND BUTTON_CONTAINER TO CONTAINER
+container.appendChild(button_container)
+// DEFINE  THE SCORE AND APPEND IT TO CONTAINER 
+const score = document.createElement('div')
+score.classList.add('score')
+score.innerText = "Let's Play ! Pick a Tool :"
+container.insertBefore(score,button_container)
+
 function getComputerChoice(){
     let choicesArray = ['Rock','Paper','Scissors'];
     let randomIndex = Math.floor(Math.random() * choicesArray.length);
@@ -12,19 +33,38 @@ function getPlayerChoice(event){
 }
 function playGame(){
     let rounds = 1;
+    let playerScore = 0;
+    let computerScore = 0;
     buttons.forEach((button) => {
         button.addEventListener('click',function(event){
-            if(rounds<6){
-                console.log("Round "+rounds)
+            if(rounds<=6){
                 let playerChoice =  getPlayerChoice(event);
                 let computerChoice =  getComputerChoice(event);
                 let result = playOneRound(playerChoice,computerChoice)
-                console.log("Player : "+playerChoice)
-                console.log("Computer : "+computerChoice)
-                console.log("Result for " + playerChoice + " vs " + computerChoice + ": " + result);
+                
+                if(result === 'Player Wins !' ){
+                    playerScore++;
+                } else if (result === 'Computer Wins !'){
+                    computerScore++;
+                } else{
+                    // nothing happens
+                }
+                score.innerHTML = "Round "+rounds+"<br> "+"Player : "+playerChoice+"<br>Computer : "+computerChoice+
+                                  "<br>Score : "+playerScore+" - "+computerScore;
                 rounds++;
             }
-            if(rounds===6){
+            if(rounds===7){
+                score.setAttribute('style','text-align:center;')
+                if(playerScore>computerScore){
+                    score.innerHTML = "Soory <br>Game Over ! <br>5 Rounds played ! <br>"+"<br>Score : "+playerScore+" - "+computerScore+
+                                "<br>"+"Player Wins !"
+                } else if(playerScore<computerScore){
+                    score.innerHTML = "5 Rounds played ! <br>"+"<br>Score : "+playerScore+" - "+computerScore+
+                                "<br>"+"Computer Wins !"
+                } else {
+                    score.innerHTML = "5 Rounds played ! <br>"+"<br>Score : "+playerScore+" - "+computerScore+
+                                "<br>"+"Tie Game !"
+                }
                 buttons.forEach(button => {
                     button.removeEventListener('click', this);
                 });
@@ -57,5 +97,5 @@ function playOneRound(playerSelection, computerSelection){
         }
     }
 }
-
 playGame();
+
